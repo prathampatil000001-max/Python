70b3758f52428a7ad5794ac64dbbe5612ee3fe6b
prathampatil000001-max <pratham.bonde.cg@gmail.com>
2026-08-23 10:53:20 +0530

    Added Assignment.

---
+++ Assignment.py
@@ -0,0 +1,43 @@
+#Question-24
+my_name="Pratham patil"
+Student_age=17
+city_name="khamgaon"
+print("student_name,student_age,student_city")
+
+
+#Question-25
+Student_name="Pratham patil"
+student_Roll_number=30
+student_branch="computer science and engineering"
+print("student_name,student_Roll_number,student_branch")
+
+#Question-26
+mark=400
+mark=420
+print(marks)
+print("the value of mark before reassigmentis 420.")
+
+#Question-27
+Name="Rahul"
+age=18
+city=Patna 
+print("name,age,city")
+
+
+#Question-28
+x=0
+y=0
+z=0
+print("x,y,z")
+
+
+#Question-29
+student="Rahul"
+student_name="rahul"
+branch=B.TECH
+
+#Question-30
+student_name2="pratham patil"
+student_age=20
+student_age=19
+print("student_name2,student_age")
