f2969b7062189b38e0bd76d4f3a24c95a8bfea4b
prathampatil000001-max <pratham.bonde.cg@gmail.com>
2026-08-24 18:10:14 +0530

    Asignment completed 3

---
+++ Python class3/Assignment.py
@@ -0,0 +1,77 @@
+#Question number24:
+
+Name="Pratham patil"
+Age=17
+height=5.6
+Person_student=True
+Rusult=None
+print(type(Name))
+print(type(Age))
+print(type(height))
+print(type(Person_student))
+print(type(Rusult))
+
+#Question number25:
+
+a=50
+b=50.0
+c="50"
+print(type(a))
+print(type(b))
+print(type(c))
+
+#Question number26:
+
+a=True
+b="True"
+print(type(a))
+print(type(b))
+
+
+
+#Question number27:
+a=None
+b="None"
+print(type(a))
+print(type(b))
+
+#Question number28:
+
+Marks=90
+print(type(Marks))
+Marks="90"
+print(type(Marks))
+
+#Question number29:
+Product="Mouse"
+print(type(Product))
+Price=5000.00
+print(type(Price))
+Product=10
+print(type(Product))
+is_availabe=True
+print(type(is_availabe))
+Discount=None
+print(type(Discount))
+
+#Question number30:
+
+Number1=10
+print(type(Number1))
+Marks=10.0
+print(type(Marks))
+marks="10"
+print(type(marks))
+student_available=True
+print(type(student_available))
+Name="True"
+print(type(Name))
+Student_available=None
+print(type(Student_available))
+student_name="None"
+print(type(student_name))
+
+
+
+
+
---
+++ Python class3/demo.py
@@ -0,0 +1,9 @@
+age=22
+age=str(22)
+print( age, type(age))
+# Another demo 
+Name="Pratham"
+Age=22
+AGe="22" 
+Info="Here Age has integer value and AGe has string value"
+
